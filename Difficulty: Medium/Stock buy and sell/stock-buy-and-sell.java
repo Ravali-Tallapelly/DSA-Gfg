10109612e348d863716f9e2d65f890384c181afb
Ravali-Tallapelly <166210373+Ravali-Tallapelly@users.noreply.github.com>
2025-07-20 17:27:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.java
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.java
@@ -1,12 +1,11 @@
 class Solution {
     int stockBuySell(int arr[]) {
-        // code here
-        int totalProfit=0;
+        int profit=0;
         for(int i=1;i<arr.length;i++) {
             if(arr[i]>arr[i-1]) {
-                totalProfit+=(arr[i]-arr[i-1]);
+                profit+=(arr[i]-arr[i-1]);
             }
         }
-        return totalProfit;
+        return profit;
     }
 }

eb9f0abbd0798514a95a522fea6c0f45a05ae438
Ravali-Tallapelly <166210373+Ravali-Tallapelly@users.noreply.github.com>
2025-08-28 16:46:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseWords(String s) {
+        int n=s.length();
+        int last=n-1;
+        String res="";
+        for(int right=n-1;right>=0;right--) {
+            if(s.charAt(right)!='.') {
+                continue;
+            }
+            int left=right+1;
+            while(right>=0 && s.charAt(right)=='.') {
+                right-=1;
+            }
+            res+=s.substring(left,last+1);
+            if(res.length()>0) {
+                res+='.';
+            }
+            last=right;
+        }
+        if(last>=0) {
+            res+=s.substring(0,last+1)+'.';
+        }
+        return res.substring(0,res.length()-1);
+    }
+}

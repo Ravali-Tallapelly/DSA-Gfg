5e421cbd96102e19932f39a4fc3dc5d02ad7448f
Ravali-Tallapelly <166210373+Ravali-Tallapelly@users.noreply.github.com>
2026-02-08 21:44:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap two numbers/swap-two-numbers.py
+++ Difficulty: Basic/Swap two numbers/swap-two-numbers.py
@@ -1,6 +1,7 @@
 class Solution:
     def get(self, a: int, b: int) -> tuple[int, int]:
-        a=a^b
-        b=a^b
-        a=a^b
-        return (a,b)
+        a=a+b 
+        b=a-b 
+        a=a-b
+        return [a,b]
+        
